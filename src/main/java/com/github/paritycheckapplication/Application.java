2adc3a945d14613428f6afd9653f772c125410cc
ge93yim <bjoern.kremser@tum.de>
2020-08-06 10:34:01 +0200

    Fix structure in pom.xml and add Parity-Check-Matrix-1.1.jar

--- src/main/java/com/github/paritycheckapplication/Application.java
+++ src/main/java/com/github/paritycheckapplication/Application.java
@@ -1,3 +1,5 @@
+package com.github.paritycheckapplication;
+
 public class Application {
     public static void main(String[] args) {
         //Those values are used if no valid arguments are in args
--- src/main/java/com/github/paritycheckapplication/ParityCheck.java
+++ src/main/java/com/github/paritycheckapplication/ParityCheck.java
@@ -1,3 +1,5 @@
+package com.github.paritycheckapplication;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
